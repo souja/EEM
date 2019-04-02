1aafbb24e412d932bceed60377f5a5fb549d354e
hikobe8 <hikobe8@163.com>
2019-04-02 22:34:25 +0800

    optimize expandable recycler

--- Eem/app/src/main/java/com/byt/eem/act/ActMyProjects.kt
+++ Eem/app/src/main/java/com/byt/eem/act/ActMyProjects.kt
@@ -4,12 +4,11 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.SparseBooleanArray
 import android.view.View
 import android.view.ViewGroup
-import com.byt.eem.R
+import com.byt.eem.*
 import com.byt.eem.base.BaseAct
-import com.byt.eem.inflate
-import com.byt.eem.toggle
 import kotlinx.android.synthetic.main.activity_act_my_projects.*
 import kotlinx.android.synthetic.main.item_project.view.*
 
@@ -33,30 +32,42 @@ class ActMyProjects : BaseAct() {
 
 class ProjectAdapter:RecyclerView.Adapter<ProjectAdapter.ProjectHolder>(){
 
+    private val arr = SparseBooleanArray()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =  ProjectHolder(parent.inflate(R.layout.item_project))
 
     override fun getItemCount(): Int {
-        return 4
+        return 40
     }
 
     override fun onBindViewHolder(holder: ProjectHolder, position: Int) {
-
-
+        holder.bindView(arr.get(position, false))
+        holder.itemView.iv_toggle.setOnClickListener {
+            if (holder.itemView.ll_content_drawer.isVisible()) {
+                arr.delete(position)
+            } else {
+                arr.put(position, true)
+            }
+            notifyItemChanged(position)
+        }
 
     }
 
     class ProjectHolder(view:View):RecyclerView.ViewHolder(view){
 
         init {
-            itemView.iv_toggle.setOnClickListener {
-                itemView.ll_content_drawer.toggle(true)
+        }
+
+        fun bindView(open:Boolean){
+            if (open) {
+                itemView.ll_content_drawer.setVisibility(true)
+            } else {
+                itemView.ll_content_drawer.setVisibility(false, true)
             }
         }
 
-        fun bindView(){
-
-        }
-
     }
+
+
 
 }
