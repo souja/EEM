a5b5c70fc49a6d88cb513153fc8c468f65cf6755
yangdezhao <782579195@qq.com>
2019-04-06 14:39:40 +0800

    123
    12

---
+++ Eem/app/src/main/java/com/byt/eem/act/ActProvinceProjects.java
@@ -0,0 +1,16 @@
+package com.byt.eem.act;
+
+import com.byt.eem.R;
+import com.byt.eem.base.BaseAct;
+
+public class ActProvinceProjects extends BaseAct {
+    @Override
+    protected int setupViewRes() {
+        return R.layout.act_province_projects;
+    }
+
+    @Override
+    protected void initMain() {
+
+    }
+}
